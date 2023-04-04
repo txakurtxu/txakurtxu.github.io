d68f4ba8f4962f16075c10fd86de68a17c65a5e9
txakurtxu <61714085+txakurtxu@users.noreply.github.com>
2023-04-04 12:09:05 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,17 @@
 
 int main(void)  {
   printf("Hello World!\n");
+  
+  FILE *arch;
+  if((arch=fopen("README.md","r"))==NULL)  {
+    printf("Oops, could not open README.md\nExiting ...\n");
+    return 1;
+  }
+  char ctmp;
+  while(fread(&ctmp,sizeof(char),1,arch))>0)  {
+    printf("%c",ctmp);
+  }
+  fclose(arch);
+  
   return 0;
 }
